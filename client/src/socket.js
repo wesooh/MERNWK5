8ce5cba58d6e8f6ea762cc21754ec33505926d6d
wesooh <bsit594j2022@students.tum.ac.ke>
2025-07-02 15:03:29 +0300

    Remove img.png

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // Adjust this URL if your backend is hosted elsewhere
-const socket = io('http://localhost:5000');
+const socket = io('https://vercel.com/wesleys-projects-2a2d1438/mernwk-5/Bxaogvy2dJU82vSoy6bkCKcbNmcT');
 
 export default socket;
